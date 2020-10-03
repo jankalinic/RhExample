cc340e8852e1041e57c5a44394310ec588bebed0
HonzaKali <work.kalinic@gmail.com>
2020-10-03 04:48:41 +0200

    fixed dependencies first test working

--- src/main/java/redhatExample/VertxServer.java
+++ src/main/java/redhatExample/VertxServer.java
@@ -1,23 +1,33 @@
 package redhatExample;
+
+import io.vertx.core.*;
+import io.vertx.core.http.*;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpMethod;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.UUID;
 
+
 public class VertxServer extends AbstractVerticle{
 
-    public void start() throws UnknownHostException {
-        Vertx vertx = Vertx.vertx();
+    private final Vertx vertx = Vertx.vertx();
+
+
+    @Override
+    public void start(Promise<Void> promise) throws UnknownHostException {
+
         String uniqueID = UUID.randomUUID().toString();
         String currentHostname =  InetAddress.getLocalHost().getHostName();
 
-        vertx.createHttpServer().requestHandler(req -> {
+        HttpServer server = vertx.createHttpServer(new HttpServerOptions().setHost("localhost").setPort(8088));
+        server.requestHandler(req -> {
             if (req.method() == HttpMethod.GET) {
-                req.response().end("hello from server ID: <" + uniqueID + "> with HOSTNAME: <" + currentHostname + ">\n");
+                req.response().end("hello from server ID: <" + uniqueID + "> with HOSTNAME: <" + currentHostname + ">");
             }
-        }).listen(8088);
+        }).listen(8088,"localhost",futureResolver->{
+            promise.complete();
+        });
     }
+
 }
--- src/test/java/VertxServerTest.java
+++ src/test/java/VertxServerTest.java
@@ -1,52 +1,78 @@
-import redhatExample.VertxServer;
 
-import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.ext.web.client.*;
 
-import org.junit.Test;
+
+import io.vertx.ext.unit.TestContext;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.net.UnknownHostException;
+import io.vertx.junit5.VertxExtension;
+import org.junit.runner.RunWith;
 
 
+import redhatExample.VertxServer;
+
+@RunWith(VertxUnitRunner.class)
+public class VertxServerTest{
 
 
-@ExtendWith(VertxExtension.class)
-public class VertxServerTest extends AbstractVerticle{
+    private static Vertx vertx = Vertx.vertx();
+
+    int port = 8088;
+    String host = "localhost";
+    String uri = "/";
 
     @BeforeAll
-    static void setUp() throws UnknownHostException{
-        VertxServer s = new VertxServer();
-        s.start();
+    public static void setUp(TestContext context) {
+        System.out.println("Running @BeforeAll setUp now");
+
+
+        vertx.deployVerticle(VertxServer.class.getName(), context.asyncAssertSuccess());
+
     }
 
+    //@Ignore
     @Test
-    public void testGet(){
-        Vertx vertx = Vertx.vertx();
+    public void testGet(TestContext context) {
+        System.out.println("Running @Test testGet now");
+
+        Async async = context.async();
         WebClient client = WebClient.create(vertx);
 
-        System.out.println("cus");
-        System.out.println(true);
+        client  .get(port, host, uri)
+                .send(ar -> {
 
-        client.get(8088,"localhost","/").send(hand->{
-            if (hand.succeeded()){
-                System.out.println("jo");
-            }
-            else{
-                System.out.println("ne");
-            }
+                if (ar.succeeded()) {
 
-        });
+                    HttpResponse<Buffer> response = ar.result();
 
-
+                    System.out.println("Odezva status Code: " + response.statusCode());
+                    System.out.println("----------SERVER RESPOND BODY--------");
+                    System.out.println(response.bodyAsString());
+                    System.out.println("-------------------------------------");
+                }
+                else {
+                    System.out.println("Chyba: " + ar.cause().getMessage());
+                }
+                async.complete();
+            });
     }
 
-   /* @Test
-    public void failTest(){
-        System.out.println("FailTest - Ok");
+    @AfterAll
+    public void tearDown(TestContext context) {
+        System.out.println("tearDown Vertx");
+        vertx.close(context.asyncAssertSuccess());
     }
-    */
+
+
 }
